c7e26a770f93196820263eceb03c21f94300bad9
Miguel Carreño <mcn0105@gmail.com>
2023-05-13 16:02:27 -0400

    Reorganizamos

--- src/service/CalculadoraService.java
+++ src/service/CalculadoraService.java
@@ -1,17 +1,21 @@
 package service;
 
+import util.Utilidades;
+
 public class CalculadoraService {
 	public int sumar(int a, int b) {
 		return a+b;
 	}
 	public int restar(int a, int b) {
 		return a-b;
+		//return Utilidades.mayor(a,  b)Utilidades.menor(a, b);
 	}
 	public int multiplicar(int a, int b) {
 		return a*b;
 	}
 	public int dividir(int a, int b) {
 		return a/b;
+		//return Utilidades.mayor(a,  b)/Utilidades.menor(a, b);
 	}
 
 }
---
+++ src/util/Utilidades.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Utilidades {
+	public static int mayor(int x, int y) {
+		return Math.max(x, y);
+	}
+	public static int menor(int x, int y) {
+		return Math.min(x, y);
+	}
+}
