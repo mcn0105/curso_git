1146234c57c8ca09410b645402e94f3ce4772ace
Miguel Carreño <mcn0105@gmail.com>
2023-05-13 16:05:27 -0400

    Añadimos nueva clase utilidades en development

--- src/service/CalculadoraService.java
+++ src/service/CalculadoraService.java
@@ -7,15 +7,15 @@ public class CalculadoraService {
 		return a+b;
 	}
 	public int restar(int a, int b) {
-		return a-b;
-		//return Utilidades.mayor(a,  b)Utilidades.menor(a, b);
+		//return a-b;
+		return Utilidades.mayor(a,  b)-Utilidades.menor(a, b);
 	}
 	public int multiplicar(int a, int b) {
 		return a*b;
 	}
 	public int dividir(int a, int b) {
-		return a/b;
-		//return Utilidades.mayor(a,  b)/Utilidades.menor(a, b);
+		//return a/b;
+		return Utilidades.mayor(a,  b)/Utilidades.menor(a, b);
 	}
 
 }
