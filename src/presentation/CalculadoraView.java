62d07af2b26d2d88a717f8e5bb0eba2325634a4c
Miguel Carreño <mcn0105@gmail.com>
2023-05-29 19:33:04 -0400

    usuario2 añade funcionalidad capa presentacion

--- src/presentation/CalculadoraView.java
+++ src/presentation/CalculadoraView.java
@@ -8,6 +8,8 @@ public class CalculadoraView {
 		CalculadoraService calc=new CalculadoraService();
 		System.out.println(calc.sumar(2, 8));
 		System.out.println(calc.multiplicar(5, 6));
+		System.out.println(calc.restar(3, 7));
+		System.out.println(calc.dividir(8, 3));
 
 	}
 
