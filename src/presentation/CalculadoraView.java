acdbe56a080ce65f12fd9925c642169e9a58cf64
Miguel Carreño <mcn0105@gmail.com>
2023-06-04 12:21:59 -0400

    usuario2 añade funcionalidad presentacion

--- src/presentation/CalculadoraView.java
+++ src/presentation/CalculadoraView.java
@@ -10,6 +10,7 @@ public class CalculadoraView {
 		System.out.println(calc.multiplicar(5, 6));
 		System.out.println(calc.restar(3, 7));
 		System.out.println(calc.dividir(8, 3));
+		System.out.println(calc.sumatorio(new int [] {7,2,9,11}));
 
 	}
 
